2e3e5f00f5b8191a7e8dc2aa236d06ce103fe4ca
Andrew Barbosa Deras <andrewderas7@gmail.com>
2022-10-06 21:13:41 -0300

    commit 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("andrew");
+const codificar = document.getElementById('codificar');
+const decodificar = document.getElementById('decodificar');
+const botao = document.getElementById('botao');
+
+function mudaTexto () {
+    if (decodificar.checked){
+        botao.innerText = "decodificar";
+    } else if (codificar.checked){
+        botao.innerText = "codificar";
+    }
+}
